eb00af9540bf112a188a5d20afd125849227c6db
CanT <taalaay@gmail.com>
2020-01-14 22:26:42 +0300

    Initial Commit

---
+++ sociLeaks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sociLeaks
+//
+//  Created by Can Talay on 14.01.2020.
+//  Copyright © 2020 Can Talay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
